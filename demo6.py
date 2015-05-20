c4b099d74464547e51ed9185f12688c2e496a25b
蔡涵镛 <caihanyong@foxmail.com>
2015-05-20 21:48:49 +0800

    Python 模块的使用

---
+++ demo6.py
@@ -0,0 +1,23 @@
+# import sys
+
+# print 'you input is:'
+# for i in sys.argv:
+#     print i
+
+# print '\n\nThe PYTHONPATH is', sys.path, '\n'
+
+
+# # # # # # # # # # # # # # # # # # # # # # # 
+
+
+# if __name__ == '__main__':
+#     print 'This program is being run by itself'
+# else:
+#     print 'I am being imported from another module'
+
+
+
+def sayhi():
+    print 'Hi, this is mymodule speaking.'
+
+version = '0.1'
---
+++ demo6_2.py
@@ -0,0 +1,17 @@
+import demo6
+
+demo6.sayhi()
+print 'Version', demo6.version
+
+
+
+# from demo6 import sayhi, version
+
+# sayhi()
+# print 'Version', version
+
+
+# from demo6 import *
+
+# sayhi()
+# print 'Version', version
